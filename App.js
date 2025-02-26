775d008c302c2e34e167d4a01d6a91d687b07ead
banhmisg9509 <banhmisg9509@gmail.com>
2025-02-26 11:58:53 +0700

    part10 exercise 10.2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>It's only a simple app.</Text>
+      <Text>It&#39s only a simple app.</Text>
       <StatusBar style="auto" />
     </View>
   );
